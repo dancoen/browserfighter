f93d38aa3ce93204cbca2fc56befae78e9c55702
dancoen <607676@bah.com>
2019-10-09 14:55:33 -0400

    Moved files to /client instead of root dir.

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const socketIo = require("socket.io");
+var P2P = require('socket.io-p2p');
+var io = require('socket.io-client');
+var socket = io();
+var p2p = new P2P(socket);
+p2p.on('peer-msg', function (data) {
+  console.log('From a peer %s', data);
+});
+
+
+
 class App extends Component {
   render() {
     return (
